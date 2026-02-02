2e21d9d89262081b92bd9c3833dac3b3029713ad
Anastasiia Puzacheva <anastasiapuzaceva@gmail.com>
2026-02-02 14:25:08 +0100

    v.8 fix after review

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -135,7 +135,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ],
-
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
@@ -144,7 +143,7 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
     'DEFAULT_PAGINATION_CLASS':
-        'rest_framework.pagination.LimitOffsetPagination',
+        'api.pagination.LimitPageNumberPagination',
     'PAGE_SIZE': 6,
 }
 
