dc2796c48970211243bbcc8065f68d623df24997
Anastasiia Puzacheva <anastasiapuzaceva@gmail.com>
2026-06-07 19:49:34 +0200

    dns

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/backend_static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'collected_static' / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
